40e986a66aa6635dadce25b3ffe9e52fd3658679
Shivam <shivamkragarwal@gmail.com>
2023-01-15 17:57:57 +0530

    map

--- lib/presentation/layouts/maps.dart
+++ lib/presentation/layouts/maps.dart
@@ -16,6 +16,26 @@ class UserCurrentLocation extends StatefulWidget {
 class _UserCurrentLocationState extends State<UserCurrentLocation> {
  final Completer<GoogleMapController> _controller = Completer();
  static final  CameraPosition _KGooglePlex =  CameraPosition(target: LatLng(51.5, -0.09),zoom: 14);
+ List<Marker> _marker = [];
+ List<Marker> _list = [
+   Marker(markerId: MarkerId('1'),
+   position: LatLng(51.5, -0.09)
+   ),
+   Marker(markerId: MarkerId('2'),
+       position: LatLng(51.4, -0.08)
+   ),
+   Marker(markerId: MarkerId('3'),
+       position: LatLng(51.6, -0.10)
+   )
+ ];
+ @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _marker.addAll(_list);
+  }
+  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,6 +44,7 @@ class _UserCurrentLocationState extends State<UserCurrentLocation> {
            _controller.complete(controller);
         },
         initialCameraPosition: _KGooglePlex,
+        markers: Set<Marker>.of(_marker),
       ),
     );
   }
